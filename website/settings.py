1f07e528c9149bb24632985044e092077f44e0a6
Jayden <sqwatato@gmail.com>
2022-09-13 23:07:43 -0700

    migration?

--- website/settings.py
+++ website/settings.py
@@ -41,6 +41,22 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com', "127.0.0.1"]
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
+
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 
