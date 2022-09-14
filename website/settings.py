b1d4a961375a74e46a354a8b6fcb04f6249b07f5
Jayden <sqwatato@gmail.com>
2022-09-13 23:11:23 -0700

    disable static

--- website/settings.py
+++ website/settings.py
@@ -70,7 +70,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'whitenoise.runserver_nostatic',
-    'django.contrib.staticfiles',
+    # 'django.contrib.staticfiles',
 ]
 
 MIDDLEWARE = [
