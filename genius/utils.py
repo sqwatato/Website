31de05d2b5a11ea8592cec0cb2fcf660492985c1
Jayden <sqwatato@gmail.com>
2022-10-13 23:07:08 -0700

    stuff

--- genius/utils.py
+++ genius/utils.py
@@ -12,7 +12,7 @@ def get_song(title, author):
         lyrics = song.lyrics.splitlines()
         for line in lyrics:
             if "You might also like" in line or "Embed" in line:
-                lyrics.remove(line)
+                lyrics[lyrics.index(line)] = line.replace("You might also like", "").replace("Embed", "")
         return lyrics
     except:
         return None
